9b0c192be0bc6b635cdc0ca202f20238920e05d9
junho1405 <junho1405@naver.com>
2025-09-10 15:45:28 +0900

    20250910 버튼

--- Game/Assets/Class5th Transform/Scripts/Button_Manager.cs
+++ Game/Assets/Class5th Transform/Scripts/Button_Manager.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI;   
+using System;
+using UnityEngine.Rendering;
+using TMPro;
 
 public class Button_Manager : MonoBehaviour
 {
     [SerializeField] Button[] buttonList;
+    [SerializeField] string[] titleList = new string[3] { "Start", "Option" , "Quit" };
     private void Start()
     {
+        Action[] actions = { Execute , Option , Quit};
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            int index = i;
+            buttonList[i].onClick.AddListener(() => actions[index]());
+            buttonList[i].GetComponentInChildren<TextMeshProUGUI>().text = titleList[index];
+        }
 
+        //buttonList[0].onClick.AddListener(Execute);
+        //buttonList[1].onClick.AddListener(Option);
+        //buttonList[2].onClick.AddListener(Quit);
     }
     public void Execute()
     {
---
+++ Game/Assets/Class5th Transform/Scripts/Select_Table.cs
@@ -0,0 +1,19 @@
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Select_Table : MonoBehaviour
+{
+    public void Enter()
+    {
+
+    }
+    public void Exit()
+    {
+
+    }
+    public void Down()
+    {
+
+    }
+
+}
