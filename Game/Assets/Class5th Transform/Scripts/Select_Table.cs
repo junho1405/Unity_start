e72dd0735f9be1c54d47b025ef375285b50c5951
junho1405 <junho1405@naver.com>
2025-09-11 15:28:28 +0900

    20250911 프리팹시스템

--- Game/Assets/Class5th Transform/Scripts/Select_Table.cs
+++ Game/Assets/Class5th Transform/Scripts/Select_Table.cs
@@ -1,18 +1,28 @@
 using System.Security.Cryptography.X509Certificates;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Select_Table : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI titleText;
+
+    private void Awake()
+    {
+        titleText = GetComponentInChildren<TextMeshProUGUI>();
+    }
     public void Enter()
     {
-
+        titleText.fontSize = 100;
     }
     public void Exit()
     {
+        titleText.fontSize = 75;
 
     }
     public void Down()
     {
+        titleText.fontSize = 50;
 
     }
 
